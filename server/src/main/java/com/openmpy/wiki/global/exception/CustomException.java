6566be3a921e77ee482c239e35ec2bdc3db38d76
openmpy <suhwan@kakao.com>
2025-08-03 16:31:08 +0900

    feat: 글로벌 예외 처리 추가 및 CustomException 구현

---
+++ server/src/main/java/com/openmpy/wiki/global/exception/CustomException.java
@@ -0,0 +1,8 @@
+package com.openmpy.wiki.global.exception;
+
+public class CustomException extends RuntimeException {
+
+    public CustomException(final String message) {
+        super(message);
+    }
+}
---
+++ server/src/main/java/com/openmpy/wiki/global/exception/ErrorResponse.java
@@ -0,0 +1,4 @@
+package com.openmpy.wiki.global.exception;
+
+public record ErrorResponse(String message) {
+}
