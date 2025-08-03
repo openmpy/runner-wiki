ed471e3178105935bd3f84fb78aae0463c3345bf
openmpy <suhwan@kakao.com>
2025-08-03 14:28:05 +0900

    feat: 서버 초기 설정

---
+++ server/src/main/java/com/openmpy/wiki/WikiApplication.java
@@ -0,0 +1,12 @@
+package com.openmpy.wiki;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WikiApplication {
+
+    public static void main(final String[] args) {
+        SpringApplication.run(WikiApplication.class, args);
+    }
+}
