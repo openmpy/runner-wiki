feb41825970d2f852fabec2b02ef0d482e9116ba
openmpy <suhwan@kakao.com>
2025-08-07 15:48:58 +0900

    style: 메인 영역의 너비 조정 및 사이드바 너비를 70에서 80으로 변경

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -29,7 +29,7 @@ export default function RootLayout({
         <Header />
         <div className="flex-1 py-5 lg:px-4">
           <div className={`flex flex-col lg:flex-row gap-5 max-w-7xl mx-auto`}>
-            <main className="w-full lg:w-4/5 p-5 bg-white border-y border-mint lg:border lg:rounded-lg lg:border-mint">
+            <main className="w-full p-5 bg-white border-y border-mint lg:border lg:rounded-lg lg:border-mint">
               {children}
             </main>
 
--- client/components/layout/Sidebar.tsx
+++ client/components/layout/Sidebar.tsx
@@ -1,7 +1,7 @@
 const Sidebar = () => {
   return (
     <aside
-      className={`w-full lg:w-70 bg-white border-y border-mint lg:border p-5 lg:rounded-lg lg:border-mint`}
+      className={`w-full lg:w-80 bg-white border-y border-mint lg:border p-5 lg:rounded-lg lg:border-mint`}
     >
       사이드바
     </aside>
