57fcfd7b66141fd22674e8860d2d6b4c1bf0e80c
openmpy <suhwan@kakao.com>
2025-08-12 16:42:02 +0900

    fix: 사이트맵 URL 수정
    - robots.ts 및 sitemap.ts 파일에서 사이트맵 URL을 "https://runner.wiki"에서 "https://www.runner.wiki"로 변경하여 일관성을 유지하고 SEO 최적화를 개선.

--- client/app/robots.ts
+++ client/app/robots.ts
@@ -6,6 +6,6 @@ export default function robots(): MetadataRoute.Robots {
       userAgent: "*",
       allow: "/",
     },
-    sitemap: "https://runner.wiki/sitemap.xml",
+    sitemap: "https://www.runner.wiki/sitemap.xml",
   };
 }
