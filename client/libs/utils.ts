1d5370a9e9af76b6b435b50f7b288fcd6d101c48
openmpy <suhwan@kakao.com>
2025-08-13 11:18:50 +0900

    chore: 테스트 배포

--- client/libs/utils.ts
+++ client/libs/utils.ts
@@ -3,37 +3,34 @@ export function formatRelativeTime(dateString: string): string {
   const now = new Date();
   const diffInSeconds = Math.floor((now.getTime() - date.getTime()) / 1000);
 
-  console.log(now.getTime(), date.getTime());
-  console.log(diffInSeconds);
-
   // 10초 미만
-  if (diffInSeconds < 10) {
-    return "방금 전";
-  }
+  // if (diffInSeconds < 10) {
+  //   return "방금 전";
+  // }
 
-  // 1분 미만 (60초)
-  if (diffInSeconds < 60) {
-    return `${diffInSeconds}초 전`;
-  }
+  // // 1분 미만 (60초)
+  // if (diffInSeconds < 60) {
+  //   return `${diffInSeconds}초 전`;
+  // }
 
-  if (diffInSeconds < 3600) {
-    const minutes = Math.floor(diffInSeconds / 60);
-    return `${minutes}분 전`;
-  }
+  // if (diffInSeconds < 3600) {
+  //   const minutes = Math.floor(diffInSeconds / 60);
+  //   return `${minutes}분 전`;
+  // }
 
-  // 24시간 미만 (86400초)
-  if (diffInSeconds < 86400) {
-    const hours = Math.floor(diffInSeconds / 3600);
-    return `${hours}시간 전`;
-  }
+  // // 24시간 미만 (86400초)
+  // if (diffInSeconds < 86400) {
+  //   const hours = Math.floor(diffInSeconds / 3600);
+  //   return `${hours}시간 전`;
+  // }
 
   // 24시간 이상이면 년월일 시분초 형식으로 표시
-  const year = date.getFullYear();
-  const month = String(date.getMonth() + 1);
-  const day = String(date.getDate());
-  const hours = String(date.getHours());
-  const minutes = String(date.getMinutes());
-  const seconds = String(date.getSeconds());
+  const year = now.getFullYear();
+  const month = String(now.getMonth() + 1);
+  const day = String(now.getDate());
+  const hours = String(now.getHours());
+  const minutes = String(now.getMinutes());
+  const seconds = String(now.getSeconds());
 
   return `${year}년 ${month}월 ${day}일 ${hours}시 ${minutes}분 ${seconds}초`;
 }
@@ -42,9 +39,6 @@ export function formatRelativeTime2(dateString: string): string {
   const date = new Date(dateString);
   const now = new Date();
   const diffInSeconds = Math.floor((now.getTime() - date.getTime()) / 1000);
-
-  console.log(now.getTime(), date.getTime());
-  console.log(diffInSeconds);
 
   // 10초 미만
   if (diffInSeconds < 10) {
