f9ef2b0ae8cc5f734fe038d6718758e73d4e280f
openmpy <suhwan@kakao.com>
2025-08-15 12:42:52 +0900

    feat: 어드민 페이지 홈 컴포넌트 수정
    - 홈 컴포넌트의 내용을 "대문"과 설명 텍스트로 변경하여 사용자에게 더 나은 정보를 제공하도록 개선.

--- admin/app/page.tsx
+++ admin/app/page.tsx
@@ -1,3 +1,8 @@
 export default function Home() {
-  return <div>Hello World</div>;
+  return (
+    <div>
+      <h1 className="font-bm-hanna text-2xl mb-6">대문</h1>
+      <p className="text-sm text-gray-500">런너위키 어드민 페이지입니다.</p>
+    </div>
+  );
 }
