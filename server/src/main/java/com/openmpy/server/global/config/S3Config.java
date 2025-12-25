3b12bd5abeffdaab9f3b7dd6d04414dd5cdc5bcc
openmpy <suhwan@kakao.com>
2025-12-25 22:02:57 +0900

    feat: S3 클라이언트 및 Presigner 설정 추가 및 서버 초기화 구성

---
+++ server/src/main/java/com/openmpy/server/global/config/S3Config.java
@@ -0,0 +1,56 @@
+package com.openmpy.server.global.config;
+
+import com.openmpy.server.global.properties.S3Properties;
+import java.net.URI;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.S3Configuration;
+import software.amazon.awssdk.services.s3.presigner.S3Presigner;
+
+@RequiredArgsConstructor
+@Configuration
+public class S3Config {
+
+    private final S3Properties s3Properties;
+
+    @Bean
+    public S3Client s3Client() {
+        final StaticCredentialsProvider provider = StaticCredentialsProvider.create(
+                AwsBasicCredentials.create(s3Properties.accessKey(), s3Properties.secretKey())
+        );
+
+        final S3Configuration s3Configuration = S3Configuration.builder()
+                .pathStyleAccessEnabled(true)
+                .build();
+
+        return S3Client.builder()
+                .region(Region.of(s3Properties.region()))
+                .credentialsProvider(provider)
+                .endpointOverride(URI.create(s3Properties.endpoint()))
+                .serviceConfiguration(s3Configuration)
+                .build();
+    }
+
+    @Bean
+    public S3Presigner s3Properties() {
+        final StaticCredentialsProvider provider = StaticCredentialsProvider.create(
+                AwsBasicCredentials.create(s3Properties.accessKey(), s3Properties.secretKey())
+        );
+
+        final S3Configuration s3Configuration = S3Configuration.builder()
+                .pathStyleAccessEnabled(true)
+                .build();
+
+        return S3Presigner.builder()
+                .region(Region.of(s3Properties.region()))
+                .credentialsProvider(provider)
+                .endpointOverride(URI.create(s3Properties.endpoint()))
+                .serviceConfiguration(s3Configuration)
+                .build();
+    }
+}
---
+++ server/src/main/java/com/openmpy/server/global/properties/S3Properties.java
@@ -0,0 +1,13 @@
+package com.openmpy.server.global.properties;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties(prefix = "s3")
+public record S3Properties(
+        String endpoint,
+        String accessKey,
+        String secretKey,
+        String bucket,
+        String region
+) {
+}
