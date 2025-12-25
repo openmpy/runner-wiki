41e9a128357a814c1fbe2d94856a2f0d4e7105a6
openmpy <suhwan@kakao.com>
2025-12-25 17:04:28 +0900

    refactor: 문서 생성/수정 시 콘텐츠 바이트 크기 계산 로직 추가

--- server/src/main/java/com/openmpy/server/document/application/DocumentService.java
+++ server/src/main/java/com/openmpy/server/document/application/DocumentService.java
@@ -9,6 +9,7 @@ import com.openmpy.server.document.domain.entity.Document;
 import com.openmpy.server.document.domain.repository.DocumentRepository;
 import com.openmpy.server.global.util.ClientIpUtil;
 import jakarta.servlet.http.HttpServletRequest;
+import java.nio.charset.StandardCharsets;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -30,7 +31,7 @@ public class DocumentService {
                 request.category(),
                 request.author(),
                 request.content(),
-                0L,
+                getContentByteSize(request.content()),
                 ClientIpUtil.getClientIp(servletRequest)
         );
         final Document savedDocument = documentRepository.save(document);
@@ -51,7 +52,7 @@ public class DocumentService {
                 request.author(),
                 request.content(),
                 documentVersion,
-                0L,
+                getContentByteSize(request.content()),
                 ClientIpUtil.getClientIp(servletRequest)
         );
         return new DocumentUpdateResponse(document.getId());
@@ -68,5 +69,9 @@ public class DocumentService {
         return documentRepository.findById(documentId).orElseThrow(
                 () -> new IllegalArgumentException("찾을 수 없는 문서 번호입니다.")
         );
+    }
+
+    private long getContentByteSize(final String content) {
+        return content.getBytes(StandardCharsets.UTF_8).length;
     }
 }
