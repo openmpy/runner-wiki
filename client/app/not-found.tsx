56656e8e6e95b43d9928f6d5b5760412cd0dc49a
openmpy <suhwan@kakao.com>
2025-12-29 05:30:35 +0900

    fix: NotFound 컴포넌트의 링크 스타일 수정
    - NotFound 컴포넌트에서 홈으로 가는 링크의 패딩을 조정하여 버튼의 클릭 영역을 확대하고 사용자 경험을 개선

--- client/app/not-found.tsx
+++ client/app/not-found.tsx
@@ -6,7 +6,7 @@ export default function NotFound() {
       <h2 className="text-2xl font-bmhanna">페이지가 존재하지 않습니다.</h2>
       <Link
         href="/"
-        className="font-bmhanna text-white rounded-sm hover:opacity-90 cursor-pointer transition-opacity text-lg px-2 py-1 bg-mint"
+        className="font-bmhanna text-white rounded-sm hover:opacity-90 cursor-pointer transition-opacity text-lg px-3 py-1 bg-mint"
       >
         홈으로
       </Link>
