cee3a7cb50dc4df795a5990f95e83c5c7a49ea0a
openmpy <suhwan@kakao.com>
2025-12-29 19:04:23 +0900

    feat: 홈 페이지에 문서 제목, 이미지 배너 및 안내 섹션 추가
    - DocumentTitle 컴포넌트를 사용하여 페이지 제목 설정
    - 외부 링크와 이미지 배너 추가
    - '런너위키란?', '안내', '문의' 섹션을 포함하여 사용자 정보 제공

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,3 +1,59 @@
+import DocumentTitle from "@/components/document/DocumentTitle";
+import Image from "next/image";
+import Link from "next/link";
+
 export default function Home() {
-  return <div>Hello World</div>;
+  return (
+    <div>
+      <DocumentTitle text="대문" />
+
+      <div className="flex flex-col gap-6">
+        <div>
+          <Link
+            href="https://tr.rhaon.co.kr/home"
+            target="_blank"
+            className="mb-2 inline-block"
+          >
+            <Image
+              src="/images/banner.jpg"
+              alt="테일즈런너 배너"
+              width={1002}
+              height={400}
+              priority
+            />
+          </Link>
+        </div>
+
+        <div className="flex flex-col gap-2">
+          <h2 className="text-lg lg:text-xl font-bmhanna">런너위키란?</h2>
+          <p className="text-sm leading-relaxed">
+            누구나 익명으로 문서를 작성하고 편집할 수 있으며 유저, 길드에 대한
+            정보를 자유롭게 기록할 수 있습니다.
+          </p>
+        </div>
+
+        <div className="flex flex-col gap-2">
+          <h2 className="text-lg lg:text-xl font-bmhanna">안내</h2>
+          <p className="text-sm leading-relaxed">
+            문서 작업 시 <strong>IP 주소가 기록</strong>되며, 이는 스팸, 악성
+            이용, 시스템 악용 방지를 위한 용도로만 사용됩니다.
+            <br />
+            해당 정보는 그 외의 목적으로 절대 활용되지 않습니다.
+          </p>
+        </div>
+
+        {/* 문의 */}
+        <div className="flex flex-col gap-2">
+          <h2 className="text-lg lg:text-xl font-bmhanna">문의</h2>
+          <p className="text-sm leading-relaxed">
+            서비스 관련 문의, 오류 제보, 문서 삭제 요청 등은{" "}
+            <Link href="mailto:runnerwiki@proton.me" className="text-mint">
+              #이메일
+            </Link>
+            을 통해 연락해 주시기 바랍니다.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 }
