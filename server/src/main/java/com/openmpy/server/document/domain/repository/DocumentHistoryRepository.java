460fa64e7ff4c2cc9ac508e217f130fce86f0bc9
openmpy <suhwan@kakao.com>
2026-01-05 15:07:37 +0900

    refactor: DocumentHistory 조회 메서드에 document 정보를 포함하도록 EntityGraph 추가

--- server/src/main/java/com/openmpy/server/document/domain/repository/DocumentHistoryRepository.java
+++ server/src/main/java/com/openmpy/server/document/domain/repository/DocumentHistoryRepository.java
@@ -4,12 +4,14 @@ import com.openmpy.server.document.domain.model.DocumentHistory;
 import java.util.Optional;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 public interface DocumentHistoryRepository extends JpaRepository<DocumentHistory, Long> {
 
+    @EntityGraph(attributePaths = "document")
     Page<DocumentHistory> findAllByDocumentId(final Long documentId, final Pageable pageable);
 
     @Query("""
