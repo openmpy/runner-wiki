04cd1bd44ca3c71550471b9b199485d8b0d3ecfc
openmpy <suhwan@kakao.com>
2026-01-05 15:26:13 +0900

    refactor: footer 런너위키 링크 추가

--- client/components/layout/Footer.tsx
+++ client/components/layout/Footer.tsx
@@ -1,9 +1,20 @@
+import Link from "next/link";
+
 export default function Footer() {
   return (
     <footer className="bg-mint p-4">
       <div className="max-w-7xl mx-auto">
         <p className="text-center text-white text-xs lg:text-sm">
-          &copy; 2025 런너위키. All rights reserved.
+          &copy; 2026{" "}
+          <Link
+            href="https://open.kakao.com/o/gtQPMY9h"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="font-semibold hover:underline"
+          >
+            런너위키.
+          </Link>{" "}
+          All rights reserved.
         </p>
       </div>
     </footer>
