38099381093f7acfe4de33bb25d3d2c23b69a669
openmpy <suhwan@kakao.com>
2026-01-05 15:03:00 +0900

    feat: DocumentHistory 조회에 document 정보를 포함하는 메서드 추가
    - `DocumentHistoryRepository`에 `findByIdWithDocument` 쿼리 메서드 추가
    - `DocumentQueryService`의 `getHistory` 메서드 로직 업데이트
    - 문서 기록 조회 시 연관된 문서 정보를 함께 반환하도록 개선

--- server/src/main/java/com/openmpy/server/document/application/query/DocumentQueryService.java
+++ server/src/main/java/com/openmpy/server/document/application/query/DocumentQueryService.java
@@ -30,9 +30,8 @@ public class DocumentQueryService {
 
     @Transactional(readOnly = true)
     public DocumentGetResponse getHistory(final Long documentHistoryId) {
-        final DocumentHistory documentHistory = documentHistoryRepository.findById(documentHistoryId).orElseThrow(
-                () -> new CustomException("찾을 수 없는 문서 기록 번호입니다.")
-        );
+        final DocumentHistory documentHistory = documentHistoryRepository.findByIdWithDocument(documentHistoryId)
+                .orElseThrow(() -> new CustomException("찾을 수 없는 문서 기록 번호입니다."));
 
         return DocumentGetResponse.from(documentHistory.getDocument(), documentHistory);
     }
--- server/src/main/java/com/openmpy/server/document/domain/repository/DocumentHistoryRepository.java
+++ server/src/main/java/com/openmpy/server/document/domain/repository/DocumentHistoryRepository.java
@@ -1,11 +1,23 @@
 package com.openmpy.server.document.domain.repository;
 
 import com.openmpy.server.document.domain.model.DocumentHistory;
+import java.util.Optional;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface DocumentHistoryRepository extends JpaRepository<DocumentHistory, Long> {
 
     Page<DocumentHistory> findAllByDocumentId(final Long documentId, final Pageable pageable);
+
+    @Query("""
+            SELECT dh
+            FROM DocumentHistory dh
+            JOIN FETCH dh.document d
+            WHERE dh.id = :id
+            """)
+    Optional<DocumentHistory> findByIdWithDocument(@Param("id") final Long id);
+
 }
