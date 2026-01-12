61719757698b7c09d4df633592381a8fee37204f
openmpy <suhwan@kakao.com>
2026-01-12 12:46:06 +0900

    fix: 중복되는 코드 제거

--- server/src/main/java/com/openmpy/server/document/application/command/DocumentImageScheduler.java
+++ server/src/main/java/com/openmpy/server/document/application/command/DocumentImageScheduler.java
@@ -5,7 +5,6 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -15,7 +14,6 @@ public class DocumentImageScheduler {
     private final DeleteAllDocumentImagesUseCase deleteAllDocumentImagesUseCase;
 
     @Scheduled(cron = "0 0 0 * * ?")
-    @Transactional
     public void execute() {
         final long count = deleteAllDocumentImagesUseCase.execute();
 
