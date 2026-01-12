8065d9e6cd5c70c0317b75e415d6532a1825c797
openmpy <suhwan@kakao.com>
2026-01-12 11:24:35 +0900

    feat: 초성 추출 유틸리티 추가

---
+++ server/src/main/java/com/openmpy/server/global/util/KoreanChosung.java
@@ -0,0 +1,41 @@
+package com.openmpy.server.global.util;
+
+public final class KoreanChosung {
+
+    private KoreanChosung() {
+    }
+
+    private static final char HANGUL_BASE = 0xAC00; // '가'
+    private static final char HANGUL_LAST = 0xD7A3; // '힣'
+    private static final int CHOSUNG_INTERVAL = 21 * 28;
+
+    private static final char[] CHOSUNG = {
+            'ㄱ', 'ㄲ', 'ㄴ', 'ㄷ', 'ㄸ', 'ㄹ', 'ㅁ', 'ㅂ', 'ㅃ', 'ㅅ', 'ㅆ', 'ㅇ', 'ㅈ', 'ㅉ', 'ㅊ', 'ㅋ', 'ㅌ', 'ㅍ', 'ㅎ'
+    };
+
+    public static String toChosung(final String input) {
+        if (input == null || input.isBlank()) {
+            return "";
+        }
+
+        final StringBuilder sb = new StringBuilder(input.length());
+
+        for (int i = 0; i < input.length(); i++) {
+            final char c = input.charAt(i);
+
+            if (Character.isWhitespace(c)) {
+                continue;
+            }
+            if (c >= HANGUL_BASE && c <= HANGUL_LAST) {
+                final int syllableIndex = c - HANGUL_BASE;
+                final int chosungIndex = syllableIndex / CHOSUNG_INTERVAL;
+
+                sb.append(CHOSUNG[chosungIndex]);
+                continue;
+            }
+
+            sb.append(c);
+        }
+        return sb.toString();
+    }
+}
